7355124403a32f1c6ae0fa86f8f2fc7cb8262c27
Md. Abid Raihan <abidcsedu@gmail.com>
2024-01-22 19:02:25 +0600

    first commit

---
+++ food-villa/src/components/Header/Title.jsx
@@ -0,0 +1,8 @@
+import food_img from '../../assets/food_villa.jpg'
+const Title = () => {
+  return (
+    <a href="/"><img src={food_img} alt="food villa" className='logo' /></a>
+  )
+}
+
+export default Title
